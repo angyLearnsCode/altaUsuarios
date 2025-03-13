7478532c57841b2893af46a82036fde9b050067d
Ángela Donoso <angeladonoso12@gmail.com>
2025-03-13 17:28:56 +0100

    Creación del interfaz de usuarip
    Y del metodo getAll

--- src/app/interfaces/iuser.interface.ts
+++ src/app/interfaces/iuser.interface.ts
@@ -1,2 +1,8 @@
 export interface IUser {
+  id?: number;
+  first_name: string;
+  last_name: string;
+  username: string;
+  email: string;
+  image: string;
 }
--- src/app/services/users-service.service.ts
+++ src/app/services/users-service.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersServiceService {
+  private http = inject(HttpClient);
+  private baseUrl: string = 'https://peticiones.online/api/users';
 
-  constructor() { }
+  getAll(): any {
+    return this.http.get<any>(this.baseUrl);
+  }
 }
