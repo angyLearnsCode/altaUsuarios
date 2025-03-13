4504955dc9d546b85618ba54db860df07a0087e2
Ángela Donoso <angeladonoso12@gmail.com>
2025-03-13 16:04:15 +0100

    Creación de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ViewUserComponent } from './pages/view-user/view-user.component';
+import { NewUserComponent } from './pages/new-user/new-user.component';
+import { UpdateUserComponent } from './pages/update-user/update-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'user/:id', component: ViewUserComponent },
+  { path: 'newuser', component: NewUserComponent },
+  { path: 'updateuser/:id', component: UpdateUserComponent },
+];
