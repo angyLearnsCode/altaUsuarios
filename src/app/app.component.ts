500135414a6af3f34ada43c135e54c94b8f93b85
Ángela Donoso <angeladonoso12@gmail.com>
2025-03-14 12:45:44 +0100

    Creación de formulario y formcontrol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/pages/new-user/new-user.component.ts
+++ src/app/pages/new-user/new-user.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-user',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './new-user.component.html',
-  styleUrl: './new-user.component.css'
+  styleUrl: './new-user.component.css',
 })
 export class NewUserComponent {
+  userForm: FormGroup;
 
+  constructor() {
+    this.userForm = new FormGroup(
+      {
+        first_name: new FormControl('', []),
+        last_name: new FormControl('', []),
+        email: new FormControl('', []),
+        username: new FormControl('', []),
+        password: new FormControl('', []),
+        repitepassword: new FormControl('', []),
+      },
+      []
+    );
+  }
+  getDataForm() {}
 }
