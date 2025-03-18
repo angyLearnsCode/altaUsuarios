1a8f4ad5ae2765a7eb85a2ddf605663cfafa3528
Ángela Donoso <angeladonoso12@gmail.com>
2025-03-18 17:32:35 +0100

    Cambio menor

--- src/app/pages/new-user/new-user.component.ts
+++ src/app/pages/new-user/new-user.component.ts
@@ -35,20 +35,20 @@ export class NewUserComponent {
     }
     this.userForm = new FormGroup(
       {
-        _id: new FormControl(this.user._id || null, []),
-        first_name: new FormControl(this.user.first_name || '', [
+        _id: new FormControl(this.idUser || null, []),
+        first_name: new FormControl(this.user?.first_name || '', [
           Validators.required,
           Validators.minLength(3),
         ]),
-        last_name: new FormControl(this.user.last_name || '', [
+        last_name: new FormControl(this.user?.last_name || '', [
           Validators.required,
           Validators.minLength(3),
         ]),
-        email: new FormControl(this.user.email || '', [
+        email: new FormControl(this.user?.email || '', [
           Validators.required,
           Validators.pattern(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/),
         ]),
-        image: new FormControl(this.user.image || '', [Validators.required]),
+        image: new FormControl(this.user?.image || '', [Validators.required]),
       },
       []
     );
